1f64e8acc8df4649bed216d462b2838689037d29
Agas Yanpratama <agas.yanpratama@gmail.com>
2018-05-11 17:33:39 +0700

    Make WebScrapper Implementation

--- src/main/java/oriconbd/utilities/WebScrapper.java
+++ src/main/java/oriconbd/utilities/WebScrapper.java
@@ -1,12 +1,59 @@
 package oriconbd.utilities;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
 
 
 public class WebScrapper {
     //TODO IMPLEMENT THIS
+    /*
+    public static void main(String[]args) throws IOException {
+        WebScrapper webScrapper = new WebScrapper();
+        System.out.println(webScrapper.WebScrapperGetter("https://www.oricon.co.jp/rank/bd/w/2018-05-07/"));
+    }
+    */
+
+    public WebScrapper() {
+
+    }
+
+    public String WebScrapperGetter(String url) throws IOException{
+        try {
+            Document document = Jsoup.connect(url).get();
+            Elements bdMovie = document.select("section.box-rank-entry");
+            return  "Top 10 Oricon BD as requested \n"
+                    +bdMovie.stream().map(this::bluRayOutputManager)
+                    .collect(Collectors.joining("\n"));
+        }
+        catch (HttpStatusException e) {
+            return "Invalid URL, Please check Your Input";
+        }
+    }
+
+    private String bluRayOutputManager(Element rawBDRank) {
+        String rankingBD = rawBDRank.select("p.num").text();
+        Element informationBD = rawBDRank.selectFirst("div.wrap-text");
+        String movieTitle = informationBD.select("h2.title").text();
+        String movieProducer = informationBD.select("p.name").text();
+        String movieReleaseDate = dateConvert(informationBD.select("li").text());
+        String output = String.format("(%s) %s - %s - %s", rankingBD
+        ,movieTitle,movieProducer,movieReleaseDate);
+        return output;
+    }
+
+    private String dateConvert (String rawDate) {
+        return  rawDate.replace("発売日： ","")
+                .replace("年", "-")
+                .replace("月", "-")
+                .replace("日", "");
+    }
 }
+
+
