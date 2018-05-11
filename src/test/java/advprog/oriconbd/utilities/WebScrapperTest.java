b23407810ade6e4b3358a08dce2573e32091f6c9
Agas Yanpratama <agas.yanpratama@gmail.com>
2018-05-11 18:56:18 +0700

    add weeklyRankTest and fix WebScrapperTest

--- src/test/java/advprog/oriconbd/utilities/WebScrapperTest.java
+++ src/test/java/advprog/oriconbd/utilities/WebScrapperTest.java
@@ -1,41 +1,18 @@
 package advprog.oriconbd.utilities;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.Before;
-import org.junit.Test;
 import oriconbd.utilities.WebScrapper;
 
 import java.io.IOException;
 
 public class WebScrapperTest {
-    private String expectedOut = "Top 10 Oricon BD as requested \n"
-            + "(1) スター・ウォーズ/最後のジェダイ MovieNEX(初回版) - マーク・ハミル"
-            + " - 2018-04-25 ウォルト・ディズニー・ジャパン\n"
-            + "(2) ラブライブ!サンシャイン!! 2nd Season 5【特装限定版】 - アニメーション"
-            + " - 2018-04-24 バンダイナムコアーツ\n"
-            + "(3) SHOGO HAMADA ON THE ROAD 2015-2016“Journey of a Songwriter”"
-            + " - 浜田省吾 - 2018-04-25 SME Records\n"
-            + "(4) ラブライブ!サンシャイン!! Aqours 2nd LoveLive! HAPPY PARTY TRAIN TOUR Blu-ray Memorial BOX"
-            + " - Aqours - 2018-04-25 ランティス\n"
-            + "(5) THE IDOLM@STER SideM GREETING TOUR 2017 〜BEYOND THE DREAM〜"
-            + " LIVE Blu-ray - アイドルマスタ"
-            + "ーSideM - 2018-04-25 ランティス\n"
-            + "(6) スター・ウォーズ/最後のジェダイ 4K UHD MovieNEX - マーク・ハミル - 2018-04-25 ウォルト・ディズニー・ジャパン\n"
-            + "(7) A3! FIRST Blooming FESTIVAL【Blu-ray】 - - - 2018-04-25 ポニーキャニオン\n"
-            + "(8) アイドリッシュセブン Blu-ray 3【特装限定版】 - アニメーション - 2018-04-24 バンダイナムコアーツ\n"
-            + "(9) アウトレイジ 最終章 - ビートたけし - 2018-04-24 バンダイナムコアーツ\n"
-            + "(10) アイドルマスター SideM 5(完全生産限定版) - アニメーション - 2018-04-25 アニプレックス";
 
-    private String invalidOutput = "Invalid URL, Please check Your Input";
     private WebScrapper webScrapper;
-    private String validUrl = "https://www.oricon.co.jp/rank/bd/w/2018-05-07/";
-    private String invalidUrl = "https://www.oricon.co.jp/rank/bd/w/2018-05-08/";
-
-    public WebScrapperTest() throws IOException {
-
-    }
 
     @Before
     public void setUp() {
@@ -49,13 +26,33 @@ public class WebScrapperTest {
 
     @Test
     public void testRankingValidUrl() throws IOException {
+        String validUrl = "https://www.oricon.co.jp/rank/bd/w/2018-05-07/";
         String validRankExp = webScrapper.webScrapperGetter(validUrl);
-        assertEquals(validRankExp,expectedOut);
+        String expectedOut = "Top 10 Oricon BD as requested \n"
+                + "(1) スター・ウォーズ/最後のジェダイ MovieNEX(初回版) - マーク・ハミル"
+                + " - 2018-04-25 ウォルト・ディズニー・ジャパン\n"
+                + "(2) ラブライブ!サンシャイン!! 2nd Season 5【特装限定版】 - アニメーション"
+                + " - 2018-04-24 バンダイナムコアーツ\n"
+                + "(3) SHOGO HAMADA ON THE ROAD 2015-2016“Journey of a Songwriter”"
+                + " - 浜田省吾 - 2018-04-25 SME Records\n"
+                + "(4) ラブライブ!サンシャイン!! Aqours 2nd LoveLive! HAPPY PARTY TRAIN TOUR Blu-ray Memorial BOX"
+                + " - Aqours - 2018-04-25 ランティス\n"
+                + "(5) THE IDOLM@STER SideM GREETING TOUR 2017 〜BEYOND THE DREAM〜"
+                + " LIVE Blu-ray - アイドルマスタ"
+                + "ーSideM - 2018-04-25 ランティス\n"
+                + "(6) スター・ウォーズ/最後のジェダイ 4K UHD MovieNEX - マーク・ハミル - 2018-04-25 ウォルト・ディズニー・ジャパン\n"
+                + "(7) A3! FIRST Blooming FESTIVAL【Blu-ray】 - - - 2018-04-25 ポニーキャニオン\n"
+                + "(8) アイドリッシュセブン Blu-ray 3【特装限定版】 - アニメーション - 2018-04-24 バンダイナムコアーツ\n"
+                + "(9) アウトレイジ 最終章 - ビートたけし - 2018-04-24 バンダイナムコアーツ\n"
+                + "(10) アイドルマスター SideM 5(完全生産限定版) - アニメーション - 2018-04-25 アニプレックス";
+        assertEquals(validRankExp, expectedOut);
     }
 
     @Test
-    public void testRankingInvalidURL() throws IOException {
+    public void testRankingInvalidUrl() throws IOException {
+        String invalidUrl = "https://www.oricon.co.jp/rank/bd/w/2018-05-08/";
         String invalidRank = webScrapper.webScrapperGetter(invalidUrl);
+        String invalidOutput = "Invalid URL, Please check Your Input";
         assertEquals(invalidOutput,invalidRank);
     }
 }
